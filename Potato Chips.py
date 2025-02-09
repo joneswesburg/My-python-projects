fb4855df4bea2e27430355305cc99c96bdd7b553
joneswesburg <144757910+joneswesburg@users.noreply.github.com>
2025-02-09 15:30:51 -0500

    Create Potato Chips.py

---
+++ Potato Chips.py
@@ -0,0 +1,7 @@
+num = input("Do you like Potato Chips? y/n")
+if num == "n":
+    print("You have committed a sin..")
+elif num == "y":
+    print("Good")
+else:
+    print("I said y/n not whatever you want.")
