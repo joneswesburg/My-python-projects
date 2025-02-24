fa84f7224456ac7f4770c600230382ef7c90019d
joneswesburg <144757910+joneswesburg@users.noreply.github.com>
2025-02-24 17:07:33 -0500

    Update Potato Chips.py
    Added some stuff

--- Potato Chips.py
+++ Potato Chips.py
@@ -1,6 +1,10 @@
 num = input("Do you like Potato Chips? y/n ")
 if num == "n":
     print("You have committed a sin..")
+elif num == "jo mama":
+    print("Uno Reverse")
+elif num == "your mom":
+    print("Uno Reverse")
 elif num == "y":
     print("Good")
 else:
