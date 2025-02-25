f8f68e47767153610d5303474c72a8a43f2c47c0
joneswesburg <144757910+joneswesburg@users.noreply.github.com>
2025-02-25 10:46:41 -0500

    Update Calculator.py
    I fixed something where when it asked you division addition multiplication or subtraction and you put the answer as uppercase it would error

--- Calculator.py
+++ Calculator.py
@@ -7,9 +7,17 @@ sub = num1 - num2
 um = input("Addition, Subtraction, Multiplication, or Division: ")
 if um == "addition":
     print(f"{num1} + {num2} = {add}")
+elif um == "Addition":
+    print(f"{num1} + {num2} = {add}")
 elif um == "subtraction":
+    print(f"{num1} - {num2} = {sub}")
+elif um == "Subtraction":
     print(f"{num1} - {num2} = {sub}")
 elif um == "multiplication":
     print(f"{num1} x {num2} = {mul}")
+elif um == "Multiplication":
+    print(f"{num1} x {num2} = {mul}")
 elif um == "division":
+    print(f"{num1} ÷ {num2} = {div}")
+elif um == "Division":
     print(f"{num1} ÷ {num2} = {div}")
